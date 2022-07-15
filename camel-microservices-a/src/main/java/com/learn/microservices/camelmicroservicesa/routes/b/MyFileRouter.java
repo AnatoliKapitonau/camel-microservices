bd77420f31640d4518df142fb9c321ff521fa58e
Anatoli Kapitonau <aka@estem.de>
2022-07-15 19:22:09 +0300

    Creating Complex Deciders for Choice in Camel Route

--- camel-microservices-a/src/main/java/com/learn/microservices/camelmicroservicesa/routes/b/MyFileRouter.java
+++ camel-microservices-a/src/main/java/com/learn/microservices/camelmicroservicesa/routes/b/MyFileRouter.java
@@ -1,10 +1,22 @@
 package com.learn.microservices.camelmicroservicesa.routes.b;
 
+import org.apache.camel.Body;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class MyFileRouter extends RouteBuilder {
+
+    @Autowired
+    private DeciderBean deciderBean;
+
     @Override
     public void configure() throws Exception {
         from("file:files/input")
@@ -13,12 +25,13 @@ public class MyFileRouter extends RouteBuilder {
                 .choice()
                     .when(simple("${file:ext} == 'xml'"))
                         .log("XML FILE")
-                    .when(simple("${body} contains 'USD'"))
+//                    .when(simple("${body} contains 'USD'"))
+                    .when(method(deciderBean))
                         .log("Not an XML FILE, BUT contains USD")
                     .otherwise()
                         .log("Not an XML FILE")
                 .end()
-                .to("direct://log-file-values")
+//                .to("direct://log-file-values")
                 .to("file:files/output");
 
         from("direct:log-file-values")
@@ -27,5 +40,17 @@ public class MyFileRouter extends RouteBuilder {
                 .log("${file:onlyname.noext} ${file:parent} ${file:path} ${file:absolute}")
                 .log("${file:size} ${file:modified}")
                 .log("${routeId} ${camelId} ${body}");
+    }
+}
+
+@Component
+class DeciderBean {
+    Logger logger = LoggerFactory.getLogger(DeciderBean.class);
+
+    public boolean isThisConditionMet(@Body String body,
+                                      @Headers Map<String, String> headers,
+                                      @ExchangeProperties Map<String, String> exchangeProperties) {
+        logger.info(" DeciderBean {} {} {}", body, headers, exchangeProperties);
+        return true;
     }
 }
